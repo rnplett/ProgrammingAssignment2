f86452112611d4576120a40cda60d01c1eb10798
rnplett <roland@rnplett.org>
2014-07-22 19:38:22 -0600

    tested vesion 1 - cleaned up comments

--- cachematrix.R
+++ cachematrix.R
@@ -38,16 +38,4 @@ cacheSolve <- function(x, ...) {
         inv
 }
 
-## Notes from Community TA on testing
-## 
-## amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
-## amatrix$get()         # Returns original matrix
-## cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
-## amatrix$getinverse()  # Returns matrix inverse
-## cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
-## 
-## amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
-## cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
-## amatrix$get()         # Returns matrix
-## amatrix$getinverse()  # Returns matrix inverse
 
